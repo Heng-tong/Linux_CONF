02fe0334fe535f356bff159ee9362dd3055405cb
Heng-tong Zhang <hengtong90@gmail.com>
2015-10-27 11:02:27 -0400

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,4 +1,3 @@
 sudo apt-get install git build-essential vim
-sudo apt-get clean
-sudo apt-get autoclean
-sudo apt-get autoremove
+# Set default java in debian
+update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_05/bin/java 100
