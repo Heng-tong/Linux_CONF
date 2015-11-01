e8bab63a4367bf31a29d5f45c1b90da5b741f03f
Heng-tong Zhang <hengtong90@gmail.com>
2015-11-01 12:42:50 -0500

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,5 +1,7 @@
 sudo apt-get install git build-essential vim
 # Set default java in debian:
-update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_05/bin/java 100
+update-alternatives --install /usr/bin/java java /usr/lib/jdk1.8.0_65/bin/java 100
+update-alternatives --install /usr/bin/javac javac /usr/lib/jdk1.8.0_65/bin/javac 100
+update-alternatives --install /usr/bin/jar jar /usr/lib/jdk1.8.0_65/bin/jar 100
 # For vmware player, we need to:
 sudo apt-get install linux-headers-`uname -r`
