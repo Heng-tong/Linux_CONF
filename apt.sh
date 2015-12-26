eb1fd68e92a66576bd3f18ed5f1f5ca3643c20f6
Hengtong Zhang <hengtong90@gmail.com>
2015-12-25 20:51:58 -0500

    Solve the problem on launching RStudio on Ubuntu 15.10.

--- apt.sh
+++ apt.sh
@@ -5,3 +5,5 @@ update-alternatives --install /usr/bin/javac javac /usr/lib/jdk1.8.0_65/bin/java
 update-alternatives --install /usr/bin/jar jar /usr/lib/jdk1.8.0_65/bin/jar 100
 # For vmware player, we need to:
 sudo apt-get install linux-headers-`uname -r`
+
+sudo apt-get install libgstreamer-plugins-base0.10-dev
