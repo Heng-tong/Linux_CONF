b20cc5029f20fea964fc47eb40de59965e60e99c
Hengtong Zhang <hengtong90@gmail.com>
2015-12-27 22:44:34 -0500

    Add tips for debian.

--- apt_for_debian.sh
+++ apt_for_debian.sh
@@ -1,4 +1,10 @@
 sudo apt-get install git build-essential vim
+
+# Remove pre-install useless packages.
+apt-get remove gnome-games
+apt-get remove --purge libreoffice*
+apt-get autoremove
+
 # Set default java in debian:
 update-alternatives --install /usr/bin/java java /usr/lib/jdk1.8.0_65/bin/java 100
 update-alternatives --install /usr/bin/javac javac /usr/lib/jdk1.8.0_65/bin/javac 100
@@ -6,4 +12,4 @@ update-alternatives --install /usr/bin/jar jar /usr/lib/jdk1.8.0_65/bin/jar 100
 # For vmware player, we need to:
 sudo apt-get install linux-headers-`uname -r`
 
-sudo apt-get install libgstreamer-plugins-base0.10-dev
+sudo apt-get install libgstreamer-plugins-base0.10-dev  # For RStudio
