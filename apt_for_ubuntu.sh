1c426698c0535f4446e87b390f8f8d9d064bfbe4
Hengtong Zhang <hengtong90@gmail.com>
2016-08-30 15:07:20 -0400

    Add apt_for_ubuntu.sh

---
+++ apt_for_ubuntu.sh
@@ -0,0 +1,7 @@
+sudo apt-get install git build-essential vim openssh-server
+apt-get remove --purge libreoffice*
+apt-get autoremove
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
