f6b3a300222152961821ffc51956fc49ef7c367c
Hengtong Zhang <hengtong90@gmail.com>
2016-09-14 10:42:58 -0400

    Update apt_for_ubuntu.sh

--- apt_for_ubuntu.sh
+++ apt_for_ubuntu.sh
@@ -6,6 +6,8 @@ sudo apt-get remove hexchat
 
 sudo apt-get autoremove
 
+sudo apt-get install software-properties-common python-software-properties
+
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
