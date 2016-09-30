753923a4ba8f1be3d817a24f6e427e38769e72ce
Phantom <hengtong90@gmail.com>
2016-09-30 15:46:34 -0400

    Update and rename server_awesome.sh to server_i3.sh

--- server_awesome.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install vim git build-essential xorg awesome awesome-extra lightdm-gtk-greeter
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
---
+++ server_i3.sh
@@ -0,0 +1,6 @@
+sudo apt-get install vim git build-essential xorg i3 lightdm-gtk-greeter
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+xrandr --output DP1 --mode 1920x1080 --right-of-DP2
