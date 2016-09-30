4f981ba267378c9431f24373e3d6d6b5ba0a5772
Phantom <hengtong90@gmail.com>
2016-09-30 14:46:09 -0400

    Change java installation to ppa.

--- server_awesome.sh
+++ server_awesome.sh
@@ -1,8 +1,5 @@
 sudo apt-get install vim git build-essential python-dev r-base xorg awesome awesome-extra lightdm-gtk-greeter texstudio chromium-browser
 
-sudo tar -zvxf jdk*.tar.gz -C /usr/lib
-
-sudo echo 'export JAVA_HOME=/usr/lib/jdk1.8.0_66' >> /etc/profile
-sudo echo 'export JRE_HOME=${JAVA_HOME}/jre' >> /etc/profile
-sudo echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> /etc/profile
-sudo echo 'export PATH=${JAVA_HOME}/bin:$PATH' >> /etc/profile
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
