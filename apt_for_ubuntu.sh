6b8add03457d1d31e30b1a82c86fb00bcf07dfa2
Phantom <hengtong90@gmail.com>
2016-10-15 15:40:05 -0400

    Update apt_for_ubuntu.sh

--- apt_for_ubuntu.sh
+++ apt_for_ubuntu.sh
@@ -5,7 +5,9 @@ apt-get remove pidgin -y
 apt-get remove hexchat -y
 
 apt-get install git build-essential vim openssh-server -y
-sudo apt-get install openjdk-8-jdk -y
+apt-get install openjdk-8-jdk -y
 
-sudo apt-get autoremove
+apt-get autoremove -y
+apt-get upgrade -y
+apt-get clean -y
 # http://www.ravefinity.com/p/download-ambiance-radiance-flat-colors.html
