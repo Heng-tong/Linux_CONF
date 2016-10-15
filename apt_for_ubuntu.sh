c547e97cb32b84a78ff3e4ab94e33b9b30a06b36
Phantom <hengtong90@gmail.com>
2016-10-15 15:17:59 -0400

    Update apt_for_ubuntu.sh

--- apt_for_ubuntu.sh
+++ apt_for_ubuntu.sh
@@ -1,15 +1,11 @@
-sudo apt-get install git build-essential vim openssh-server
+#!/bin/sh
 
-sudo apt-get remove --purge libreoffice*
-sudo apt-get remove pidgin
-sudo apt-get remove hexchat
+apt-get remove --purge libreoffice* -y
+apt-get remove pidgin -y
+apt-get remove hexchat -y
+
+apt-get install git build-essential vim openssh-server -y
+sudo apt-get install openjdk-8-jdk -y
 
 sudo apt-get autoremove
-
-sudo apt-get install software-properties-common python-software-properties
-
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-
-http://www.ravefinity.com/p/download-ambiance-radiance-flat-colors.html
+# http://www.ravefinity.com/p/download-ambiance-radiance-flat-colors.html
