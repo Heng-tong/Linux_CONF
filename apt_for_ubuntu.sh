b40e9c47adf0825f9473e43f93cee3dd9252215f
Phantom <hengtong90@gmail.com>
2016-10-18 13:46:42 -0400

    Replace firefox with chromium.

--- apt_for_ubuntu.sh
+++ apt_for_ubuntu.sh
@@ -6,6 +6,8 @@ apt-get remove hexchat -y
 
 apt-get install git build-essential vim openssh-server -y
 apt-get install openjdk-8-jdk -y
+apt-get install chromium-browser -y
+apt-get purge firefox* -y
 
 apt-get autoremove -y
 apt-get upgrade -y
@@ -13,3 +15,6 @@ apt-get clean -y
 # http://www.ravefinity.com/p/download-ambiance-radiance-flat-colors.html
 
 # http://askubuntu.com/questions/732967/dropbox-icon-is-not-working-xubuntu-14-04-lts-64
+
+
+
