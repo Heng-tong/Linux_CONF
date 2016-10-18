fab3afb1e2daadea7080d919c15c10baacf854be
Phantom <hengtong90@gmail.com>
2016-10-18 19:21:35 -0400

    Update server_i3.sh

--- server_i3.sh
+++ server_i3.sh
@@ -1,3 +1,9 @@
 sudo apt-get install vim git build-essential xorg i3 lightdm-gtk-greeter
 
 echo 'xrandr --output DP1 --mode 1920x1080 --left-of DP2' >> ~/.bashrc 
+
+add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
+apt-get update -y
+apt-get install ubuntu-make -y
+
+umake ide pycharm
