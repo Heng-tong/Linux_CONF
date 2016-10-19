b6318c54112a889abb3213df69aca1a8642ecbea
Hengtong Zhang <hengtong@thranduil.cse.buffalo.edu>
2016-10-18 23:26:28 -0400

    remove oracle java

--- server_i3.sh
+++ server_i3.sh
@@ -1,6 +1,3 @@
 sudo apt-get install vim git build-essential xorg i3 lightdm-gtk-greeter
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
 
 xrandr --output DP1 --mode 1920x1080 --left-of DP2
