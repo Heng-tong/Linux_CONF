3edcefeca7a3ef1cf6b0de829e7c50d171f2ef6d
Phantom <hengtong90@gmail.com>
2016-12-15 19:25:16 -0500

    Update install_packets_arch.sh

--- install_packets_arch.sh
+++ install_packets_arch.sh
@@ -1,18 +1,17 @@
 
 # Remove libre office and firefox
-pacman -R libreoffice-still 
-pacman -Rcs firefox
+sudo pacman -R libreoffice-still 
 
 # Remove unused dependencies
-pacman -R $(pacman -Qdtq)
+sudo pacman -R $(pacman -Qdtq)
 
 # Install software
-pacman -S filezilla dropbox vim git jdk8-openjdk 
+sudo pacman -S filezilla dropbox vim git jdk8-openjdk 
 
-pacman -S yaourt
+sudo pacman -S yaourt
 
 # Clear cache
-pacman -Scc
+sudo pacman -Scc
 
 wget http://more-sky.com/data/out/7/IMG_170136.png
 mv IMG_170136.png ~/Desktop/mate-community.png
