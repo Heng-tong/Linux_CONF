478bc8ff88c28bc4e46b11b1ea480790a5f08fab
Phantom <hengtong90@gmail.com>
2017-01-02 10:57:35 -0500

    Update install_packets_arch.sh

--- install_packets_arch.sh
+++ install_packets_arch.sh
@@ -13,7 +13,7 @@ sudo systemctl enable sshd
 sudo systemctl start sshd
 
 # Handle dual monitor problem in i3.
-echo 'xrandr --output DP1 --mode 1920x1080 --left-of DP2' >> ~/.xsession
+# echo 'xrandr --output DP1 --mode 1920x1080 --left-of DP2' >> ~/.xsession
 
 sudo pacman -S yaourt
 
@@ -23,9 +23,3 @@ sudo pacman -Scc
 # wget http://more-sky.com/data/out/7/IMG_170136.png
 # mv IMG_170136.png ~/Desktop/mate-community.png
 # sudo mv ~/Desktop/mate-community.png mate-community.png
-
-# Resize windows configuration for i3
-# bindsym $mod+Ctrl+Right resize shrink width 1 px or 1 ppt
-# bindsym $mod+Ctrl+Up resize grow height 1 px or 1 ppt
-# bindsym $mod+Ctrl+Down resize shrink height 1 px or 1 ppt
-# bindsym $mod+Ctrl+Left resize grow width 1 px or 1 ppt
